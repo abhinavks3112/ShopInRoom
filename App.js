321d42b3722b82478eef6bf958084e29b8b3a842
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-02 23:23:03 +0530

    Configured custom fonts for use in app

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+// Load custom fonts to be used anywhere in this app
+const fetchFonts = () => {
+  Font.loadAsync({
+    // eslint-disable-next-line global-require
+    roboto: require('./assets/fonts/Roboto-Regular.ttf'),
+    // eslint-disable-next-line global-require
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf')
+  });
+};
 
 export default function App() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  /* If font has'nt been loaded, then display splash screen */
+  if (!isFontLoaded) {
+    return (
+      <AppLoading
+      startAsync={fetchFonts}
+      onFinish={setIsFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
