47a884745fa6ef3d3b307caddccd8d9229184b2e
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-04 22:04:27 +0530

    Added a model class for items in our cart

---
+++ models/cart-item.js
@@ -0,0 +1,10 @@
+class CartItem {
+    constructor(productTitle, productPrice, quantity, sum) {
+        this.productTitle = productTitle;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+        this.sum = sum;
+    }
+}
+
+export default CartItem;
