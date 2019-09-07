706276ae9a4cd7036f1844cdd59ba4d20c7682a9
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-07 18:24:52 +0530

    Added action, reducer for adding orders

--- store/actions/types.js
+++ store/actions/types.js
@@ -1,2 +1,6 @@
+// Cart Actions
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+
+// Orders Actions
+export const ADD_ORDER = 'ADD_ORDER';
---
+++ store/reducers/ordersReducer.js
@@ -0,0 +1,27 @@
+import Order from '../../models/order';
+import { ADD_ORDER } from '../actions/types';
+
+const INITIAL_STATE = {
+    orders: []
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case ADD_ORDER: {
+            const newOrUpdatedOrder = new Order(
+                new Date().toString(),
+                action.payload.items,
+                action.payload.amount,
+                new Date().getDate()
+            );
+            return {
+                ...state,
+                /* Adds the new item to the item and return a new array,
+                so the old array remains untouched, so original remains immutable */
+                orders: state.orders.concat(newOrUpdatedOrder)
+            };
+        }
+        default:
+            return state;
+    }
+};
