6ba67a87d694405ced2e9b9eaaf9edabe99c345f
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-11 01:25:47 +0530

    Dispatched action for adding/updating products

--- screens/user/EditProductsScreen.js
+++ screens/user/EditProductsScreen.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
  View,
  TextInput,
  ScrollView,
  StyleSheet
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import CustomHeaderButton from '../../components/HeaderButton';
 import TitleText from '../../components/TitleText';
+import * as ProductAction from '../../store/actions/productsAction';
 
 const EditProductsScreen = (props) => {
     const { navigation } = props;
+    const dispatch = useDispatch();
 
     const prodId = navigation.getParam('id');
 
@@ -24,6 +26,30 @@ const EditProductsScreen = (props) => {
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
     const [price, setPrice] = useState('');
+
+    /* If we do not specify the title, description, etc in the dependency list then
+    the value for those variables inside never updates when the user changes
+    those value, they always remain blank or initial values */
+    const submitHandler = useCallback(() => {
+        if (editedProduct) {
+            dispatch(ProductAction.updateProduct(
+            prodId, title, imageUrl, description
+            ));
+        } else {
+            /* Adding unary plus if your string is already in the form of an integer:
+            + sign before price convert it from string to number */
+            dispatch(ProductAction.createProduct(
+            title, imageUrl, description, +price
+            ));
+        }
+    }, [description, dispatch, editedProduct, imageUrl, price, prodId, title]);
+
+     /* Passing function to navigationOptions below so that it can be used in onPress
+    action of save button */
+    useEffect(() => {
+        navigation.setParams({ submit: submitHandler });
+    }, [submitHandler]);
+
 
     return (
         <View style={styles.screen}>
@@ -73,18 +99,25 @@ const EditProductsScreen = (props) => {
     );
 };
 
-EditProductsScreen.navigationOptions = (navData) => ({
-        headerTitle: navData.navigation.getParam('id') ? 'Edit Product' : 'Add Product',
+EditProductsScreen.navigationOptions = (navData) => {
+    const { navigation } = navData;
+
+    const id = navigation.getParam('id');
+    const submit = navigation.getParam('submit');
+
+    return {
+        headerTitle: id ? 'Edit Product' : 'Add Product',
         headerRight: (
             <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item
                 title="Save"
                 iconName="save"
-                onPress={() => {}}
+                onPress={submit}
                 />
             </HeaderButtons>
-            )
-    });
+        )
+    };
+};
 
 const styles = StyleSheet.create({
     screen: {
