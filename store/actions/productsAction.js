ab22fe0dafae9216514ea4d4f606c429ba1a5236
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-11 01:23:54 +0530

    Added action, reducer to add/update a product

--- store/actions/productsAction.js
+++ store/actions/productsAction.js
@@ -1,6 +1,40 @@
-import { DELETE_PRODUCT } from './types';
+import {
+    DELETE_PRODUCT,
+    CREATE_PRODUCT,
+    UPDATE_PRODUCT
+ } from './types';
 
 export const deleteProduct = (productId) => ({
     type: DELETE_PRODUCT,
     payload: productId
+});
+
+export const createProduct = (
+ title,
+ imageUrl,
+ description,
+ price
+) => ({
+    type: CREATE_PRODUCT,
+    payload: {
+        title,
+        imageUrl,
+        description,
+        price
+    }
+});
+
+export const updateProduct = (
+    id,
+    title,
+    imageUrl,
+    description
+   ) => ({
+    type: UPDATE_PRODUCT,
+    payload: {
+        id,
+        title,
+        imageUrl,
+        description
+    }
 });
--- store/reducers/productsReducer.js
+++ store/reducers/productsReducer.js
@@ -1,5 +1,10 @@
 import PRODUCTS from '../../data/dummy-data.js';
-import { DELETE_PRODUCT } from '../actions/types';
+import {
+    DELETE_PRODUCT,
+    CREATE_PRODUCT,
+    UPDATE_PRODUCT
+ } from '../actions/types';
+import Product from '../../models/product';
 
 const INITIAL_STATE = {
     availableProducts: PRODUCTS,
@@ -8,6 +13,65 @@ const INITIAL_STATE = {
 
 const productsReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case CREATE_PRODUCT: {
+            const {
+                title,
+                imageUrl,
+                description,
+                price
+            } = action.payload;
+            const newProduct = new Product(
+                new Date().toString(),
+                'u1',
+                title,
+                imageUrl,
+                description,
+                parseFloat(price)
+            );
+            return {
+                ...state,
+                availableProducts: state.availableProducts.concat(newProduct),
+                userProducts: state.userProducts.concat(newProduct)
+            };
+        }
+
+        case UPDATE_PRODUCT: {
+            const {
+            id,
+            title,
+            imageUrl,
+            description
+            } = action.payload;
+
+            const productIndex = state.userProducts.findIndex((product) => product.id === id);
+            const updatedProduct = new Product(
+                id,
+                state.userProducts[productIndex].ownerId,
+                title,
+                imageUrl,
+                description,
+                // Since we are not changin price, we will keep it the same
+                parseFloat(state.userProducts[productIndex].price)
+            );
+            const updatedUserProduct = [...state.userProducts];
+            /* Updating the product located at index(that we found for the edited product)
+            with the list of updated fields  */
+            updatedUserProduct[productIndex] = updatedProduct;
+
+            /* Doing the same thing for available Products array */
+            const availableProductsIndex = state.availableProducts.findIndex(
+                (product) => product.id === id
+            );
+            const updatedAvailableProducts = [...state.availableProducts];
+            updatedAvailableProducts[availableProductsIndex] = updatedProduct;
+
+            return {
+                ...state,
+                availableProducts: updatedAvailableProducts,
+                userProducts: updatedUserProduct
+            };
+        }
+
         case DELETE_PRODUCT: {
             return ({
                 ...state,
@@ -19,6 +83,7 @@ const productsReducer = (state = INITIAL_STATE, action) => {
                     )
             });
         }
+
         default:
             return state;
     }
