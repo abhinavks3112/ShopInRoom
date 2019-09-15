74c247c5291cb334b9ba55510257e7df17b797b0
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-15 22:58:38 +0530

    Posted data on Firebase server using redux thunk

--- store/actions/productsAction.js
+++ store/actions/productsAction.js
@@ -9,20 +9,50 @@ export const deleteProduct = (productId) => ({
     payload: productId
 });
 
+/* Using redux thunk here, we now instead of returning an object or action,
+we return a function which receive dispatch as argument, inside of this function,
+we dispatch action object */
 export const createProduct = (
  title,
  imageUrl,
  description,
  price
-) => ({
-    type: CREATE_PRODUCT,
-    payload: {
-        title,
-        imageUrl,
-        description,
-        price
-    }
-});
+) =>
+    /* Using async await syntax for readability.
+    In background, it translate to fetch().then() syntax */
+    async (dispatch) => {
+    // Any async code here
+    const response = await fetch('https://shopinroom-65e62.firebaseio.com/products.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        /* Firbase automatically generate an id for us,
+        so no need to send that. */
+        body: JSON.stringify({
+            title,
+            imageUrl,
+            description,
+            price
+        })
+    });
+
+    /* We have to put await in on every async promise */
+    const responseData = await response.json();
+
+    console.log(responseData);
+
+    dispatch({
+        type: CREATE_PRODUCT,
+        payload: {
+            id: responseData.name, // Firebase generated unique id for the record
+            title,
+            imageUrl,
+            description,
+            price
+        }
+    });
+};
 
 export const updateProduct = (
     id,
